d8d06b82c63a3d42c514b7689a06005a8a1f5bab
thiagosnx <thiagosantanacq@gmail.com>
2023-09-16 02:21:31 -0300

    adicionando codigos

---
+++ src/main/java/com/emailservice/core/EmailSenderUseCase.java
@@ -0,0 +1,6 @@
+package com.emailservice.core;
+
+public interface EmailSenderUseCase {
+    void sendEmail(String to, String subject, String body); //o que a nossa aplicacao faz
+
+}
---
+++ src/main/java/com/emailservice/infra/ses/AwsSesConfig.java
@@ -0,0 +1,16 @@
+package com.emailservice.infra.ses;
+
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsSesConfig {
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService(){
+        return AmazonSimpleEmailServiceClientBuilder.standard().build();
+    }
+}
